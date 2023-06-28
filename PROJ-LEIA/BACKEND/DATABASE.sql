899dec6dfa600316855cf2c209e86dd4d647f979
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2023-06-28 09:27:32 -0300

    DATABASE - GRUPS

--- PROJ-LEIA/BACKEND/DATABASE.sql
+++ PROJ-LEIA/BACKEND/DATABASE.sql
@@ -1,5 +1,5 @@
-create database registros;
-use registros;
+CREATE DATABASE registros;
+USE registros;
 
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -7,17 +7,42 @@ CREATE TABLE usuarios (
     email VARCHAR(45),
     password VARCHAR(200)
 );
+SELECT * FROM usuarios; 
 
-select * from usuarios;
-
-CREATE TABLE projetos (
+CREATE TABLE team (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT,
+    id_usuarios INT,
+    titulo TEXT,
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios (id)
+);
+SELECT * FROM team; 
+
+CREATE TABLE folder (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    id_teams INT,
+    titulo TEXT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id),
+    FOREIGN KEY (id_teams) REFERENCES team (id)
+);
+SELECT * FROM folder;
+
+
+CREATE TABLE projetos /*ANTIGA PROJETOS*/(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    id_folder INT ,
     titulo TEXT,
     content TEXT,
     preview VARCHAR(1000),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id),
+    FOREIGN KEY (id_folder) REFERENCES folder (id)
 );
-select * from projetos;
+SELECT * FROM projetos; 
 
-drop database registros;
+DROP TABLE projetos ;
+DROP TABLE folder;
+DROP TABLE team;
+DROP TABLE usuarios;
+
+DROP DATABASE registros;
