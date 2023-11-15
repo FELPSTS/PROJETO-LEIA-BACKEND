7f2e508b5b5ad29db7485c847c5b37c542250dc5
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2023-11-14 22:29:00 -0300

    Update DATABASE.sql

--- PROJ-LEIA/BACKEND/DATABASE.sql
+++ PROJ-LEIA/BACKEND/DATABASE.sql
@@ -7,37 +7,40 @@ CREATE TABLE usuarios (
     email VARCHAR(45) NOT NULL,
     icon_user BLOB,
     password VARCHAR(200) NOT NULL
-    Verified BOOLEAN DEFAULT FALSE,	
 );
 SELECT * FROM usuarios; 
-
-/*
-CREATE TABLE admins(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	userid INT,
-	FOREIGN KEY (userid) REFERENCES usuarios (id)
-);
-SELECT * FROM admins;
-*/
 
 CREATE TABLE team(
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_usuarios INT,
     titulo TEXT NOT NULL,
+    content TEXT NOT NULL,
     team_icon BLOB,
     FOREIGN KEY (id_usuarios) REFERENCES usuarios (id)
 );
 SELECT * FROM team; 
 
-CREATE TABLE teams_usuario(
+CREATE TABLE teams(
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_time INT,
     id_usuario INT,
     FOREIGN KEY (id_time) REFERENCES team (id),
     FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
 );
-SELECT * FROM teams_usuario;
-insert into teams_usuario (id_time, id_usuario) values (1,3);
+SELECT * FROM teams; 
+
+CREATE TABLE invitation(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_time INT,
+    id_destinatário INT,
+    id_remetente INT,			
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    aceito INT DEFAULT 0,	
+    FOREIGN KEY (id_time) REFERENCES teams (id),
+    FOREIGN KEY (id_destinatário) REFERENCES usuarios (id),
+    FOREIGN KEY (id_remetente) REFERENCES usuarios (id)
+);
+SELECT * FROM invitation; 
 
 CREATE TABLE project (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -75,26 +78,21 @@ CREATE TABLE docs (
 );
 SELECT * FROM docs;
 
-CREATE TABLE invites(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_time INT,
-    id_destinatario INT,
-    id_remetente INT,			
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    aceito BOOLEAN DEFAULT FALSE,	
-    FOREIGN KEY (id_time) REFERENCES team (id),
-    FOREIGN KEY (id_destinatario) REFERENCES usuarios (id),
-    FOREIGN KEY (id_remetente) REFERENCES usuarios (id)
-);
-SELECT * FROM invites; 
+DELIMITER //
+CREATE TRIGGER after_invite_update
+AFTER UPDATE ON invitation
+FOR EACH ROW
+BEGIN
+	IF NEW.aceito = 2 THEN
+    DELETE FROM invitation WHERE id = NEW.id;
+END IF;
+END;
+// DELIMITER ;
 
-drop table invites;
 DROP TABLE docs;
 DROP TABLE folder;
 DROP TABLE team;
-DROP TABLE teams_usuario;
 DROP TABLE docs;
 DROP TABLE usuarios;
-/* DROP TABLE admins; */
 
 DROP DATABASE registros;
